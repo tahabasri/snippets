dce82606529c167582e9fcf3b7b0aae92b8dfad3
Taha Basri <tahabsri@gmail.com>
2023-09-02 16:38:54 +0100

    Revert "Add tab support for text-area"
    This reverts commit 30073a3de03efd2cde27513005928694b996be5b.

--- views/js/editSnippet.js
+++ views/js/editSnippet.js
@@ -1,19 +1,4 @@
 (function() {
-
-    const snippetValue = document.getElementById('snippet-value');
-    snippetValue.addEventListener('keydown', keyHandler, false);
-
-    function keyHandler(e) {
-        var TABKEY = 9;
-        if(e.keyCode === TABKEY) {
-            this.value += "\t";
-            if(e.preventDefault) {
-                e.preventDefault();
-            }
-            return false;
-        }
-    }
-
     const vscode = acquireVsCodeApi();
 
     const resolveSyntaxCB = document.lastChild.querySelector('input[name="_snippets_resolve_syntax"]');
